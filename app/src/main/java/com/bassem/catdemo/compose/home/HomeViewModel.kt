0a3089e2cd1d134ce85573f86acabd5e5a98e502
Bassem <BasemOmar2012@gmail.com>
2024-10-04 15:40:01 +0100

    create HomeViewModel

---
+++ app/src/main/java/com/bassem/catdemo/compose/home/HomeViewModel.kt
@@ -0,0 +1,31 @@
+package com.bassem.catdemo.compose.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bassem.catdemo.data.models.Result
+import com.bassem.catdemo.data.repo.CatRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(private val repo: CatRepo) : ViewModel() {
+
+    private var _breedsList = MutableStateFlow<Result?>(null)
+    val breedsList: Flow<Result> get() = _breedsList.filterNotNull()
+
+    init {
+        fetchBreedsList()
+    }
+
+    private fun fetchBreedsList() = viewModelScope.launch {
+        repo.getCatsBreeds().collect { result ->
+            _breedsList.value = result
+
+        }
+    }
+
+}
