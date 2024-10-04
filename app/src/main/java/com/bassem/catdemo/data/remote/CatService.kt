0a8950d1de3edef0e3b652826b0f93a461275417
Bassem <BasemOmar2012@gmail.com>
2024-10-04 12:03:03 +0100

    create CatService.kt and its di

---
+++ app/src/main/java/com/bassem/catdemo/data/remote/CatService.kt
@@ -0,0 +1,25 @@
+package com.bassem.catdemo.data.remote
+
+import com.bassem.catdemo.data.models.BreedItem
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface CatService {
+    @GET("breeds?limit=10&page=0")
+    suspend fun getCatsBreeds(): BreedItem
+
+    companion object {
+        private const val BASE_URL = "https://api.thecatapi.com/v1/"
+
+        fun create(): CatService {
+            val log = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+            val client = OkHttpClient.Builder().addInterceptor(log).build()
+            return Retrofit.Builder().baseUrl(BASE_URL).client(client)
+                .addConverterFactory(GsonConverterFactory.create()).build()
+                .create(CatService::class.java)
+        }
+    }
+}
---
+++ app/src/main/java/com/bassem/di/NetWorkModule.kt
@@ -0,0 +1,16 @@
+package com.bassem.di
+
+import com.bassem.catdemo.data.remote.CatService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class NetWorkModule {
+    @Singleton
+    @Provides
+    fun provideCatApi(): CatService = CatService.create()
+}
