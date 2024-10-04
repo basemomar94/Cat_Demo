78d7568dfad604d99496e39d6dab87f25316b16d
Bassem <BasemOmar2012@gmail.com>
2024-10-04 11:33:54 +0100

    create compose of HomeScreen

--- app/src/main/java/com/bassem/catdemo/compose/home/HomeScreen.kt
+++ app/src/main/java/com/bassem/catdemo/compose/home/HomeScreen.kt
@@ -1,9 +1,45 @@
 package com.bassem.catdemo.compose.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import com.bassem.catdemo.data.models.BreedItem
+import com.bassem.catdemo.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(){
-   Text(text = "Home Screen")
+fun HomeScreen() {
+    val breedsList = listOf<BreedItem>()
+    Scaffold(topBar = { TopAppBar(title = { Text(text = "Cat Breeds") }) }) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+
+            SearchBar(query = "", onQueryChange = {})
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(dimensionResource(id = R.dimen.default_padding))
+            ) {
+                items(breedsList) { item: BreedItem ->
+                    BreedListItem(item, onClick = {})
+
+                }
+            }
+
+        }
+
+    }
 }
---
+++ app/src/main/java/com/bassem/catdemo/compose/home/SearchBar.kt
@@ -0,0 +1,39 @@
+package com.bassem.catdemo.compose.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import com.bassem.catdemo.R
+
+@Composable
+fun SearchBar(query: String, onQueryChange: (String) -> Unit) {
+    TextField(
+        value = query, onValueChange = onQueryChange, modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                dimensionResource(id = R.dimen.default_padding),
+            )
+            .background(
+                color = MaterialTheme.colorScheme.surface, shape = RoundedCornerShape(
+                    dimensionResource(id = R.dimen.card_corner_radius)
+                )
+            ), leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = stringResource(id = R.string.search_icon)
+            )
+        }
+    )
+
+
+}
