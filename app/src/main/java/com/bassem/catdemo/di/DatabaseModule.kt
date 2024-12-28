91dde71fbaebe277e447705917d28ec53d9c3d8f
Bassem <BasemOmar2012@gmail.com>
2024-12-28 22:49:47 +0200

    update room db di

--- app/src/main/java/com/bassem/catdemo/di/DatabaseModule.kt
+++ app/src/main/java/com/bassem/catdemo/di/DatabaseModule.kt
@@ -3,8 +3,10 @@
 package com.bassem.catdemo.di
 
 import android.content.Context
+import androidx.room.Room
 import com.bassem.catdemo.data.local.AppDatabase
 import com.bassem.catdemo.data.local.CatsDao
+import com.bassem.catdemo.utils.AppConstants.DATABASE_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,7 +21,8 @@ class DatabaseModule {
     @Singleton
     @Provides
     fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
-        return AppDatabase.getInstance(context)
+        return Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
+            .build()
     }
 
     @Provides
